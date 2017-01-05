0f80863093c5528b90c64491b0542b153e17e408
Chelaris182 <Chelaris182@users.noreply.github.com>
2017-01-05 21:50:16 +0100

    AsyncDeflate classes

---
+++ AsyncDeflate/Interop.Libraries.Windows.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace AsyncDeflate
+{
+    internal static partial class Interop
+    {
+        internal static class Libraries
+        {
+            internal const string Zlib = "clrcompression.dll";
+        }
+    }
+}
---
+++ AsyncDeflate/SR.cs
@@ -0,0 +1,23 @@
+﻿namespace AsyncDeflate
+{
+    class SR
+    {
+        internal const string NotSupported = "NotSupported";
+        internal const string NotSupported_UnreadableStream = "NotSupported_UnreadableStream";
+        internal const string ArgumentOutOfRange_Enum = "ArgumentOutOfRange_Enum";
+        internal const string ObjectDisposed_StreamClosed = "ObjectDisposed_StreamClosed";
+        internal const string NotSupported_UnwritableStream = "NotSupported_UnwritableStream";
+        internal const string GenericInvalidData = "GenericInvalidData";
+        internal const string CannotReadFromDeflateStream = "CannotReadFromDeflateStream";
+        internal const string CannotWriteToDeflateStream = "CannotWriteToDeflateStream";
+        internal const string InvalidArgumentOffsetCount = "InvalidArgumentOffsetCount";
+        internal const string InvalidBeginCall = "InvalidBeginCall";
+        internal const string ZLibErrorDLLLoadError = "ZLibErrorDLLLoadError";
+        internal const string ZLibErrorUnexpected = "ZLibErrorUnexpected";
+        internal const string ZLibErrorInconsistentStream = "ZLibErrorInconsistentStream";
+        internal const string ZLibErrorNotEnoughMemory = "ZLibErrorNotEnoughMemory";
+        internal const string ZLibErrorIncorrectInitParameters = "ZLibErrorIncorrectInitParameters";
+        internal const string ZLibErrorVersionMismatch = "ZLibErrorVersionMismatch";
+        internal const string ArgumentOutOfRange_NeedPosNum = "ArgumentOutOfRange_NeedPosNum";
+    }
+}
